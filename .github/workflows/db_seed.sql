7fb1bd0e0584446233a7ccf93f50619a6ac563cf
claudiaap0 <claudia.apostu@endava.com>
2025-09-02 15:51:24 +0300

    Delete .github/workflows/db_seed.sql

--- .github/workflows/db_seed.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  email TEXT UNIQUE NOT NULL
-);
-
-INSERT INTO users (email) VALUES
-  ('alice@example.com'),
-  ('bob@example.com')
-ON CONFLICT DO NOTHING;
